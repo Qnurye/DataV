e602e6aef3bf4d9993e85ad95e18f31afdad54bf
Qnurye <Qnurye@gmail.com>
2024-11-29 23:06:36 +0800

    feat: cn map

---
+++ src/main.ts
@@ -0,0 +1,12 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import App from './App.vue'
+
+
+import ECharts from 'vue-echarts'
+import 'echarts'
+
+const app = createApp(App)
+app.component('v-chart', ECharts)
+app.mount('#app')
